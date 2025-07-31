a10f6b777c4af9738efda7a20ea7ac61bf4c3abe
deb <hxin.radin@gmail.com>
2025-07-31 16:55:40 +0200

    Updated extention.js [implemented use of vscode.Uri instead of pure path as strings]

--- extension.js
+++ extension.js
@@ -1,3 +1,5 @@
+//NOTE:
+// the setting file for vscode is not "setting.json" is it "settings.json" [it is plural]
 
 const vscode = require('vscode');
 //const { exec } = require("child_process");
@@ -5,12 +7,13 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-
+let rootPath;
+let extentionPath;
 
 async function getRootPath() { //if there are more than one workspace it will ask the user to pick one of them
-	let workspaces = vscode.workspace.workspaceFolders?.map(folder => folder.uri.path);
+	let workspaces = vscode.workspace.workspaceFolders?.map(folder => folder.uri);
 	if (workspaces.length > 1) { // note that in if statment anything morethan 0 is a 1 so no need to say > 1
-		let workspace = (await vscode.window.showWorkspaceFolderPick()).uri.path
+		let workspace = (await vscode.window.showWorkspaceFolderPick()).uri
 		return workspace
 	}
 
@@ -18,7 +21,7 @@ async function getRootPath() { //if there are more than one workspace it will as
 }
 
 async function initProject() {
-	let rootPath = await getRootPath();
+
 	//vscode.window.showInformationMessage("python3 -m venv " + rootPath + "/.venv");
 	//exec("python3 -m venv " + rootPath + "/.venv", () => {vscode.window.showInformationMessage("Python venv created")});
 
@@ -28,7 +31,7 @@ async function initProject() {
 		'python venv',            // Label shown in UI
 		'micropython-manager',     // "Source" of the task -> a label to help user understand where the task is comming from
 		new vscode.ShellExecution('python3 -m venv .venv && source .venv/bin/activate && pip install pyserial mpremote micropython-stm32-stubs', {
-			cwd: rootPath // passing the working directory where the shell command must be ran
+			cwd: rootPath.path // passing the working directory where the shell command must be ran
 		}),
 		[] // problemMatchers if any
 	);
@@ -39,18 +42,25 @@ async function initProject() {
 }
 
 async function test() {
-	let rootPath = await getRootPath();
 
 	console.log(vscode.workspace.getConfiguration("python"));
-	vscode.workspace.getConfiguration("python").update("venvPath", rootPath + "/.venv", vscode.ConfigurationTarget.workspace);
+
+	vscode.workspace.getConfiguration("python").update(
+		"venvPath",
+		vscode.Uri.joinPath(rootPath, ".venv"),
+		vscode.ConfigurationTarget.workspace
+	);
 
 	vscode.window.createTerminal().show();
 
-	vscode.window.showInformationMessage(rootPath + ".vscode")
-	vscode.workspace.fs.createDirectory(vscode.Uri.file(rootPath + ".vscode"));
+	vscode.window.showInformationMessage(
+		vscode.Uri.joinPath(rootPath, ".venv")
+	);
+
+	await vscode.workspace.fs.createDirectory(vscode.Uri.joinPath(rootPath, ".vscode"));
 
 	const vscode_setting = Buffer.from(
-`{
+		`{
 	"python.languageServer": "Pylance",
 	"python.analysis.typeCheckingMode": "basic",
 	"python.analysis.diagnosticSeverityOverrides": {
@@ -63,14 +73,32 @@ async function test() {
 	]	
 }`);
 
-	vscode.workspace.fs.writeFile(vscode.Uri.file(rootPath + "/.vscode/" + "setting.json"),vscode_setting);
+	await vscode.workspace.fs.writeFile(
+		vscode.Uri.joinPath(rootPath, ".vscode", "setting.json"),
+		vscode_setting
+	);
 }
-function activate(context) {
+
+async function test2() {
+	vscode.window.showInformationMessage(extentionPath);
+
+	await vscode.workspace.fs.copy(
+		vscode.Uri.joinPath(extentionPath, "resources", "settings.json"),
+		vscode.Uri.joinPath(rootPath, ".vscode", "settings.json")
+	)
+}
+async function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('micropython-manager.init-project', test);
 
+	rootPath = await getRootPath();
+	extentionPath = context.extensionUri; //note: the Uri object is not just a string, the string part is the .path 
+
+	console.log(rootPath.path);
+	console.log(extentionPath.path);
+
+	const disposable = vscode.commands.registerCommand('micropython-manager.init-project', test2);
 	context.subscriptions.push(disposable);
 }
 
